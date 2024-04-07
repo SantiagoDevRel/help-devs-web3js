63b04e8921368e4ef342f031bb38fea2a0b727fa
santiagodevrel <santiago@chainsafe.io>
2024-04-07 07:42:17 +0200

    updated react app

--- react/test/src/App.js
+++ react/test/src/App.js
@@ -5,17 +5,23 @@ function App() {
   const contract_address = "0xd3e7B4BF6FB03bdECbe5Db0b348285A22bF33Bf6";
 
   async function main() {
+    //CONNECT METAMASK
     const web3 = new Web3(window.ethereum);
+    //await window.ethereum.send("eth_requestAccounts");
+    const accounts = await web3.eth.requestAccounts();
+    console.log("account conneted:", accounts[0]);
 
-    const contract = new web3.eth.Contract(ABI, contract_address);
-
-    const result = await contract.methods.touristRewardOnPostID("0x1a620c351c07763f430897AeaA2883E37cA0aaCD", "0x26eecb00ddef76d58362552f4fd2e782ae49d1e064ccd5b06bd70dcd8039ec35").call();
-    console.log("result", result);
+    //INITIALIZE CONTRACT
   }
+  //donate(){}
+
+  //withdraw(){}
 
   return (
     <div className="App">
       <button onClick={main}>Connect</button>
+      <button onClick={main}>Donate</button>
+      <button onClick={main}>Withdraw</button>
     </div>
   );
 }
